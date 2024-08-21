3cb3ffe6f829ffa4a3db7986a0dc52d9f941ab6d
HasmikAbaz <36800799+HasmikAbaz@users.noreply.github.com>
2024-08-21 22:16:10 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,33 @@
-// Countdown Timer
-const weddingDate = new Date('September 20, 2024 00:00:00').getTime();
-const countdownEl = document.getElementById('countdown');
+// Comment out the scroll animation logic for now
 
-function updateCountdown() {
-    const now = new Date().getTime();
-    const timeUntilWedding = weddingDate - now;
+// const scrollElements = document.querySelectorAll("[data-scroll]");
 
-    const days = Math.floor(timeUntilWedding / (1000 * 60 * 60 * 24));
+// const elementInView = (el, dividend = 1) => {
+//     const elementTop = el.getBoundingClientRect().top;
+//     return (
+//         elementTop <= 
+//         (window.innerHeight || document.documentElement.clientHeight) / dividend
+//     );
+// };
 
-    countdownEl.innerText = days;
-}
+// const displayScrollElement = (element) => {
+//     element.classList.add("data-scroll-visible");
+// };
 
-setInterval(updateCountdown, 1000);
+// const hideScrollElement = (element) => {
+//     element.classList.remove("data-scroll-visible");
+// };
 
-// Scroll Animation
-const scrollElements = document.querySelectorAll("[data-scroll]");
+// const handleScrollAnimation = () => {
+//     scrollElements.forEach((el) => {
+//         if (elementInView(el, 1.25)) {
+//             displayScrollElement(el);
+//         } else {
+//             hideScrollElement(el);
+//         }
+//     });
+// }
 
-const elementInView = (el, dividend = 1) => {
-    const elementTop = el.getBoundingClientRect().top;
-    return (
-        elementTop <= 
-        (window.innerHeight || document.documentElement.clientHeight) / dividend
-    );
-};
-
-const displayScrollElement = (element) => {
-    element.classList.add("data-scroll-visible");
-};
-
-const hideScrollElement = (element) => {
-    element.classList.remove("data-scroll-visible");
-};
-
-const handleScrollAnimation = () => {
-    scrollElements.forEach((el) => {
-        if (elementInView(el, 1.25)) {
-            displayScrollElement(el);
-        } else {
-            hideScrollElement(el);
-        }
-    });
-}
-
-window.addEventListener("scroll", () => {
-    handleScrollAnimation();
-});
+// window.addEventListener("scroll", () => {
+//     handleScrollAnimation();
+// });
